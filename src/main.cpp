9d01ea11c011b9ceb80e3684a151192e354cdf71
oalaro <oalaro@hotmail.com>
2020-10-02 08:41:13 +0300

    Minu branch

--- src/main.cpp
+++ src/main.cpp
@@ -76,7 +76,7 @@ void processPacket(const std::string& packet)
     for (uint8_t i = 0; i < MOTOR_COUNT; i++)
     {
       float speed_setpoint = std::atof(cmd[i + 1].c_str());
-      //serial_pc.printf("Setpoint %d, %f\r\n", i, speed_setpoint);
+      serial_pc.printf("Setpoint %d, %f\r\n", i, speed_setpoint);
       m[i].setSpeedSetPoint(speed_setpoint);
     }
     cmd_timer.reset();
@@ -191,8 +191,8 @@ int main()
     for (uint8_t i = 0; i < MOTOR_COUNT; i++)
     {
       // MOTOR DEBUG
-      // serial_pc.printf("\r\n");
-//      serial_pc.printf("MOTOR %d: \r\n", i);
+        //serial_pc.printf("\r\n");
+	//serial_pc.printf("MOTOR %d: \r\n", i);
 //      serial_pc.printf("Speed[%d]: %f (%f): \r\n", i, m[i].getMeasuredSpeed(),
 //                       m[i].getSpeedSetPoint());
 //      // serial_pc.printf("Effort: %f: \r\n", m[i].getEffort());
@@ -212,9 +212,9 @@ int main()
     }
     
     // Update odometry
-    odom_.update(m[0].getMeasuredSpeed(), m[1].getMeasuredSpeed(), m[2].getMeasuredSpeed());
-    serial_pc.printf("ODOM:%f:%f:%f:%f:%f:%f\r\n", odom_.getPosX(), odom_.getPosY(),
-                     odom_.getOriZ(), odom_.getLinVelX(), odom_.getLinVelY(), odom_.getAngVelZ());
+    //odom_.update(m[0].getMeasuredSpeed(), m[1].getMeasuredSpeed(), m[2].getMeasuredSpeed());
+    //serial_pc.printf("ODOM:%f:%f:%f:%f:%f:%f\r\n", odom_.getPosX(), odom_.getPosY(),
+        //             odom_.getOriZ(), odom_.getLinVelX(), odom_.getLinVelY(), odom_.getAngVelZ());
     // Synchronize to given MAIN_DELTA_T
     wait_us(MAIN_DELTA_T*1000*1000 - main_timer.read_us());
   }
