711f1db8ed842025cb77880700f7685c8b1211b5
oalaro <oalaro@hotmail.com>
2020-10-16 14:08:44 +0300

    seminonblocking 256000 baud

--- src/main.cpp
+++ src/main.cpp
@@ -59,21 +59,27 @@ RawSerial serial_pc(USBTX, USBRX);  // tx, rx
 char serial_buf[4096];        // Buffer for incoming serial data
 volatile uint32_t serial_arrived = 0;  // Number of bytes arrived
 volatile bool packet_received_b = false;
+volatile bool reset_timer = false;
 const char * jsonsource = "{\"MS\":{\"M1\": 50,\"M2\": 50,\"M3\": 50}, \"LED\": [ 10066682, 10102972, 10319056, 10355969, 10428612, 10565919, 10717752, 10783710, 10882887, 10955120, 11127807, 11158075, 11193819, 11202752, 11551486, 11623994, 11790515, 11880182, 11931999, 11952266, 12133489, 12137829, 12293910, 12413209, 12442287, 12479550, 12521919, 12638736, 12678538, 12696715, 12873257, 13166316, 13348606, 13567725, 13850595, 13898195, 13924676, 13975870, 14025285, 14064364, 14141714, 14744475, 14744976, 15186068, 15224251, 15359457, 15409668, 15469622, 15520422, 15599931, 15727045, 15742858, 15788763, 15923926, 15978899, 16054705, 16173431, 16180072, 16431170]}";
 // For parsing command with arguments received over serial
 
 std::vector<std::string> cmd;
+
 void processJsonPacket(const char * packet)
 {   
-    t.start();
+
+  
+
    Json json ( packet, strlen (packet),128 );
-         t.stop();
-        printf("The objekt time taken was %d microseconds\n", t.read_us());
-        t.reset();
+
     if ( !json.isValidJson () )
-        {
+        {   
+            //printf("The time taken was %d microseconds\n", t.read_us());
+            //t.reset();
+            //t.start();
             serial_pc.printf("Invalid JSON");
             serial_pc.printf(packet);
+
             return;
         }
 
@@ -84,7 +90,7 @@ void processJsonPacket(const char * packet)
             return;
             
         }
-         t.start();
+
        // ROOT object should have '0' tokenIndex, and -1 parentIndex
         int motorValueIndex1 = json.findKeyIndex( "M1", 0 );
         int motorValueIndex2 = json.findKeyIndex( "M2", 0 );
@@ -97,9 +103,8 @@ void processJsonPacket(const char * packet)
         int ledArrayIndex = json.findKeyIndex( "LED", 0 );
         int ledcount = json.childCount(ledArrayIndex+1);
 
-                t.stop();
-        printf("findkey taken was %d microseconds\n", t.read_us());
-        t.reset();
+
+
 
         if ( motorValueIndex1 == -1 || motorValueIndex2 == -1 || motorValueIndex3 == -1)
         {
@@ -124,7 +129,7 @@ void processJsonPacket(const char * packet)
               const char * valueStart3  = json.tokenAddress (motorValueIndex1+1);
               int          valueLength3 = json.tokenLength (motorValueIndex1+1);
               strncpy (motorValue3, valueStart3, valueLength3 );*/
-              t.start();
+
 
               json.tokenIntegerValue(motorValueIndex1+1,mv1);
               json.tokenIntegerValue(motorValueIndex2+1,mv2); 
@@ -132,15 +137,14 @@ void processJsonPacket(const char * packet)
               m[1].setSpeedSetPoint(mv1);
               m[2].setSpeedSetPoint(mv2);
               m[3].setSpeedSetPoint(mv3);
-              t.stop();
-              printf("mv integer taken was %d microseconds\n", t.read_us()); 
-              t.reset();
+
+
 
       
 
         
               
- //             serial_pc.printf( "%d", mv1);
+              //serial_pc.printf( "%d", mv1);
  //             serial_pc.printf( "%d", mv2);
  //             serial_pc.printf( "%d \n", mv3);
           }
@@ -186,16 +190,16 @@ void processJsonPacket(const char * packet)
         }
         else
         {
-          t.start();
+
 
           if(ledArrayIndex > 0)
           {
             ws1.useII(WS2812::GLOBAL);
             ws1.setII(0xFF); 
             for (int i = 0; i < ledcount; i++){
-              //json.tokenIntegerValue(ledArrayIndex+2+i,temp_led_value);
-              //px.Set(i, temp_led_value);
-              px.Set(i,ledarr[i]);
+              json.tokenIntegerValue(ledArrayIndex+2+i,temp_led_value);
+              px.Set(i, temp_led_value);
+
 
 
 
@@ -208,12 +212,10 @@ void processJsonPacket(const char * packet)
               
 
           }
-                  t.stop();
-        printf("led libra taken was %d microseconds\n", t.read_us());
-        t.reset();
 
         }
-        
+     
+     
 }
 // This method processes a received serial packet
 void processPacket(const std::string& packet)
@@ -347,26 +349,29 @@ void check_for_timeout()
 int main()
 {
   // Initialize serial connection
-  serial_pc.baud(115200);
+  serial_pc.baud(256000);
   serial_buf[0] = '\0';
   serial_pc.attach(&pc_rx_callback);
   serial_pc.printf("**** MAIN ****\r\n");
 
   cmd_timeout_checker.attach(check_for_timeout, 0.1);
   cmd_timer.start();
-
+  t.start();
+  main_timer.start();
 
   // MAIN LOOP
   while (true)
   {
-    main_timer.reset();
-    main_timer.start();
+    
+    if (reset_timer) main_timer.reset();
+
+    t.reset();
 
 
 
 
     
-    for (uint8_t i = 0; i < MOTOR_COUNT; i++)
+/*    for (uint8_t i = 0; i < MOTOR_COUNT; i++)
     {
       // MOTOR DEBUG
         //serial_pc.printf("\r\n");
@@ -379,7 +384,7 @@ int main()
     }
 
 //    serial_pc.printf("Serial arrived: %d\r\n", serial_arrived);
-    
+   */ 
     if (packet_received_b) // packet was completeted with \r \n
     { 
      
@@ -392,14 +397,13 @@ int main()
       // do stuff
      // serial_pc.printf(cstr);
       //serial_pc.printf("suur vahe \n \n \n");
-      //t.start();
+      
     
       
-      processJsonPacket(jsonsource);
+      processJsonPacket(serial_buf);
       
-      //t.stop();
-      //printf("The time taken was %d microseconds\n", t.read_us());
       
+
 
       serial_buf[0] = '\0';
       serial_arrived = 0;
@@ -407,13 +411,25 @@ int main()
       //delete [] cstr;
       
       packet_received_b = false;
+      //serial_pc.printf(" %d microseconds\n", t.read_us());
+      
     }
     
     // Update odometry
-    //odom_.update(m[0].getMeasuredSpeed(), m[1].getMeasuredSpeed(), m[2].getMeasuredSpeed());
-    //serial_pc.printf("ODOM:%f:%f:%f:%f:%f:%f\r\n", odom_.getPosX(), odom_.getPosY(),
-        //             odom_.getOriZ(), odom_.getLinVelX(), odom_.getLinVelY(), odom_.getAngVelZ());
+    odom_.update(m[0].getMeasuredSpeed(), m[1].getMeasuredSpeed(), m[2].getMeasuredSpeed());
+    serial_pc.printf("ODOM:%f:%f:%f:%f:%f:%f\r\n", odom_.getPosX(), odom_.getPosY(),
+                     odom_.getOriZ(), odom_.getLinVelX(), odom_.getLinVelY(), odom_.getAngVelZ());
     // Synchronize to given MAIN_DELTA_T
-    wait_us(MAIN_DELTA_T*1000*1000 - main_timer.read_us());
+    if (MAIN_DELTA_T-(MAIN_DELTA_T*0.4)*1000*1000-main_timer.read_us())
+    {
+      wait_us(MAIN_DELTA_T*1000*1000 - main_timer.read_us());
+      reset_timer = true;
+      
+    }
+
+  //wait_us(MAIN_DELTA_T*1000*1000 - main_timer.read_us());
+  //serial_pc.printf(" %d microseconds\n", t.read_us());
+
+
   }
 }
