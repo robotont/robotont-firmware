f01b8219cec28b70d34a891b498773d03c1d2e04
oalaro <oalaro@hotmail.com>
2021-01-27 13:44:23 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -548,7 +548,7 @@ extern "C" void MX_SPI1_Init(void)
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
@@ -650,6 +650,7 @@ int main()
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
+  /*
     setAllPixelColor( 0, 0, 0);
     wait_us(200000);
     // red
@@ -667,7 +668,7 @@ int main()
        setPixelColor( i, 0, 0, 250 );
        wait_us(200000);
     }
-    
+    */
   }
 
 }
