01cb8720107852e16591e60f88a5927726dab650
Leonid Tšigrinski <leonid.tsigrinski@skeletontech.com>
2023-10-10 22:45:39 +0300

    Added CMD_HANDLER dummy module

--- robotont-gen-3/src/svc/cmd.c
+++ robotont-gen-3/src/svc/cmd.c
@@ -1,1 +1,12 @@
 #include "cmd.h"
+
+void cmd_init(void)
+{
+}
+
+void cmd_receiveSerialData(uint8_t *ptr_data, uint16_t lenght)
+{
+}
+void cmd_transmitSerialData(uint8_t *ptr_data, uint16_t lenght)
+{
+}
--- robotont-gen-3/src/svc/cmd.h
+++ robotont-gen-3/src/svc/cmd.h
@@ -1,4 +1,17 @@
 #ifndef __CMD_H__
 #define __CMD_H__
 
+#include <stdint.h>
+#include "usbif.h"
+
+// TODO think about proper handling
+// typedef enum {
+//     CMD_RX_SERIAL_DATA,
+//     CMD_TX_SERIAL_DATA,
+// } CmdEventType;
+
+void cmd_init(void);
+void cmd_receiveSerialData(uint8_t *ptr_data, uint16_t lenght); /* Called within ISR context from lower layer */
+void cmd_transmitSerialData(uint8_t *ptr_data, uint16_t lenght);
+
 #endif
