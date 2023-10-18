e93b55ee5a2da7082e380d95155d8fc74e52e676
Leonid Tšigrinski <leonid.tsigrinski@skeletontech.com>
2023-10-18 13:57:22 +0300

    Added CMD handler stucture

---
+++ src/svc/cmd.c
@@ -0,0 +1,36 @@
+/**
+ * @file cmd.c
+ * @brief 
+ * 
+ * @author Leonid Tšigrinski (leonid.tsigrinski@gmail.com)
+ * @copyright Copyright (c) 2023 Tartu Ülikool
+ */
+#include "cmd.h"
+
+/**
+ * @brief 
+ * 
+ */
+void cmd_init(void)
+{
+}
+
+/**
+ * @brief 
+ * 
+ * @param ptr_data 
+ * @param lenght 
+ */
+void cmd_receiveSerialData(uint8_t *ptr_data, uint16_t lenght)
+{
+}
+
+/**
+ * @brief 
+ * 
+ * @param ptr_data 
+ * @param lenght 
+ */
+void cmd_transmitSerialData(uint8_t *ptr_data, uint16_t lenght)
+{
+}
