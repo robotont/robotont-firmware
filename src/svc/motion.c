778976524328177ca358f7d926b42707884f012c
Leonid Tšigrinski <leonid.tsigrinski@skeletontech.com>
2023-10-23 23:56:45 +0300

    Added interface dummy for robot motion control

---
+++ src/svc/motion.c
@@ -0,0 +1,11 @@
+/**
+ * @file motion.c
+ * @brief 
+ * 
+ * @author Leonid Tšigrinski (leonid.tsigrinski@gmail.com)
+ * @copyright Copyright (c) 2023 Tartu Ülikool
+ */
+
+#include "motion.h"
+
+
---
+++ src/svc/motion.h
@@ -0,0 +1,17 @@
+/**
+ * @file motion.h
+ * @brief
+ *
+ * @author Leonid Tšigrinski (leonid.tsigrinski@gmail.com)
+ * @copyright Copyright (c) 2023 Tartu Ülikool
+ */
+
+#ifndef __MOTION_H__
+#define __MOTION_H__
+
+#include <stdint.h>
+
+void motion_init(void);
+void motion_receiveData();
+
+#endif
