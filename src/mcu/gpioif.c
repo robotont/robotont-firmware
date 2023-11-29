c7fbd53b5617140d9d311267d2bdd3b39f2ecacc
Leonid Tšigrinski <leonid.tsigrinski@skeletontech.com>
2023-11-29 09:33:35 +0200

    Added separate GPIO interface

---
+++ src/mcu/gpioif.c
@@ -0,0 +1,24 @@
+#include "gpioif.h"
+
+void gpioif_init(void)
+{
+    // TODO [implementation]
+    // since a lot of modules use GPIO, MX_GPIO init called in main.c inside peripheral module
+}
+
+void gpioif_writePin(GpioPinType *ptr_pin, bool is_active)
+{
+    HAL_GPIO_WritePin(ptr_pin->ptr_port, ptr_pin->pin_number, is_active);
+}
+
+bool gpioif_isActive(GpioPinType *ptr_pin)
+{
+    return (bool)HAL_GPIO_ReadPin(ptr_pin->ptr_port, ptr_pin->pin_number);
+}
+
+void gpioif_togglePin(GpioPinType *ptr_pin)
+{
+    static bool is_active = false;
+    HAL_GPIO_WritePin(ptr_pin->ptr_port, ptr_pin->pin_number, is_active);
+    is_active = !is_active;
+}
