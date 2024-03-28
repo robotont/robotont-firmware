1099186b549dffb89a74e669d0d61136075074aa
Andres Sakk <andres.sakk@ut.ee>
2024-03-28 16:47:45 +0200

    add docstring to menu.h

--- src/svc/menu.h
+++ src/svc/menu.h
@@ -1,18 +1,18 @@
 /**
- * Includes
- * Defines
- * Type definitions
- * Global Variables (extern)
+ * @file menu.h
+ * @brief Service. Displays information and allows for changing various parameters on the display using rotary encoder as input
+ *
  * 
- * Public function prototypes
- * */
-
+ * 
+ * @author Andres Sakk (andres.sakk@ut.ee)
+ * @copyright Copyright (c) 2023 Tartu Ülikool
+ */
 #ifndef MENU_H
 #define MENU_H
 
 #include "ssd1306.h"
 
-void menu_init(void);
+void menu_init();
 void menu_update();
 
 #endif
